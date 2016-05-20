1ebf6a422db7b0120dd8eb8274887965a5ffee12
albertno <albert.no0@gmail.com>
2016-05-20 00:40:04 -0700

    TST: decoder test in progress

---
+++ tests/test_CROM_full/test_CROM_full.cpp
@@ -0,0 +1,54 @@
+#include <fstream>
+#define TEST_BLOCKLENGTH 65536
+#define TEST_HALFBLOCKLENGTH 32768
+
+#include "../../encoder/CROM_encoder.hpp"
+#include "../../decoder/CROM_decoder.hpp"
+using namespace std;
+
+int main() {
+    double x[TEST_BLOCKLENGTH];
+    double xhat[TEST_BLOCKLENGTH];
+    double thetas[TEST_HALFBLOCKLENGTH];
+    double R = 0.001;
+    double doubleL;
+    int xdim = TEST_BLOCKLENGTH;
+    double n = static_cast<double> (xdim);
+    int L;
+
+    doubleL = n*R / log(n);
+    L = static_cast<int> (doubleL);
+    cout << "creating m array" << endl;
+    cout << "L = " << L << endl;
+    int *m_array = (int*) malloc(sizeof(int)*(L+1));
+    
+    cout << "reading x input" << endl;
+    ifstream x_infile;
+    x_infile.open("../input/x_input.txt");
+    
+    // read x and initialize xhat
+    int read_line_idx;
+    for (read_line_idx=0; read_line_idx<TEST_BLOCKLENGTH; read_line_idx++) {
+        x_infile >> x[read_line_idx];
+        xhat[read_line_idx] = 0;
+    }
+
+    cout << "Running CROM" << endl;
+    CROM_encoder(x, xdim, L, m_array, false);
+
+    CROM_decoder(xhat, xdim, L, m_array);
+
+    x_infile.clear();
+    x_infile.seekg(0, x_infile.beg);
+    double l2norm = 0;
+    double diff;
+    for (read_line_idx=0; read_line_idx<TEST_BLOCKLENGTH; read_line_idx++) {
+        x_infile >> x[read_line_idx];
+        diff = x[read_line_idx] - xhat[read_line_idx];
+        l2norm += (diff*diff);
+    }
+    cout << "l2-norm at the end = " << l2norm << endl;
+    free(m_array);
+    return 0;
+}
+
