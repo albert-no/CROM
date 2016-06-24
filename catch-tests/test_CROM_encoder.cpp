5af1a0eaf22b2e1815e27005bdf923a154b12f9f
albertno <albert.no0@gmail.com>
2016-06-24 00:27:33 -0700

    TST: Add unittest framework using catch, and remove all previous tests (git ignore is added)

--- catch-tests/test_CROM_encoder.cpp
+++ catch-tests/test_CROM_encoder.cpp
@@ -7,18 +7,22 @@
 #include "catch.hpp"
 #include "../encoder/CROM_encoder.hpp"
 
-TEST_CASE("small test", "[CROM_encoder]") {
+TEST_CASE("CROM_encoder small test", "[CROM_encoder]") {
     int x_dim = 8;
     std::string name = "basic_test";
     double R = 1;
-    bool verbose = true;
+    bool verbose = false;
 
     int x_iter; 
     CROM_encoder enc (name, x_dim, R, verbose);
 
     SECTION( "test 1" ) {
         // test set_x and copy_x
-        double x[8] = {2.0, 1.2, 3.2, 1.1, 0.4, 0.7, 5.0, 10.2};
+        double x[8] = {
+            1.17844, -0.365868,
+            0.980685, 1.03383,
+            1.42599, -1.04771,
+            -0.570805, 0.929175};
         enc.set_x(x);
         
         double x_copy[8];
@@ -39,21 +43,20 @@ TEST_CASE("small test", "[CROM_encoder]") {
         int *m_arr = new int[L];
         enc.copy_m_array(m_arr);
         std::vector<int> v_m_arr(m_arr, m_arr + L);
-        std::vector<int> v_m_arr_expected {3, 3, 5};
+        std::vector<int> v_m_arr_expected {2, 7, 0};
         CHECK( v_m_arr == v_m_arr_expected );
         delete[] m_arr;
 
         // check x_remainder
         double x_rem[8] = {
-            -2.901471318,
-            -5.5536843625,
-            -1.8770677173,
-            -5.429310964,
-            2.1008209927,
-            2.5955557863,
-            -1.9996325797,
-            -2.6661145525};
-
+            -0.4673188324,
+            -1.3669546712,
+            -0.9557097795,
+            -0.1965084036,
+            0.5728391238,
+            0.2799289636,
+            0.3629543445,
+            -0.6427417279};
         enc.copy_x(x_copy);
         for (x_iter=0; x_iter<x_dim; x_iter++) {
             CHECK( x_rem[x_iter] == Approx(x_copy[x_iter]).epsilon(EPSILON) );
---
+++ catch-tests/test_matrix_multiplication.cpp
@@ -0,0 +1,102 @@
+#define BLOCKLENGTH 16
+#define HALFBLOCKLENGTH 8
+#define EPSILON 1e-6
+
+#include "catch.hpp"
+#include "../utils/matrix_multiplication.hpp"
+
+using namespace std;
+
+TEST_CASE("matrix multiplication small test", "[matrix_multiplication]") {
+    double x[BLOCKLENGTH] = {
+        1.1, 1.0, 1.0, -2.1,
+        0.1, 4.0, -1.0, 4.5,
+        1.0, 2.0, 1.3, 0.1,
+        -0.2, -0.3, 0.4, 2.2};
+    double x_final_expected[BLOCKLENGTH] = {
+        1.1, 1.0, 1.0, -2.1,
+        0.1, 4.0, -1.0, 4.5,
+        1.0, 2.0, 1.3, 0.1,
+        -0.2, -0.3, 0.4, 2.2};
+
+    long x_iter;
+
+    double thetas[HALFBLOCKLENGTH] = {
+        0.1, 0.2, 0.3, 0.4,
+        0.5, -0.2, -0.7, 2.0};
+
+    double thetas_inv[HALFBLOCKLENGTH] = {
+        -0.1, -0.2, -0.3, -0.4,
+        -0.5, 0.2, 0.7, -2.0};
+
+    SECTION( "mat_idx = 0" ) {
+        int mat_idx = 0;
+
+        // matrix multiplication
+        double x_out_expected[BLOCKLENGTH] = {
+            0.9946711652, 0.5827279163,
+            0.5711602205, -1.9731699216,
+            0.1836433639, 3.8606655121,
+            -0.5071551124, -3.8731151035,
+            1.1048209236, 2.1588024865,
+            1.5374576425, -0.7256724194,
+            -0.1275739585, -1.0886972965,
+            0.9501545622, 3.1763153803};
+
+        butterfly_matrix_multiplication(x,
+                                        thetas,
+                                        HALFBLOCKLENGTH,
+                                        0,
+                                        0,
+                                        mat_idx);
+        for (x_iter=0; x_iter<BLOCKLENGTH; x_iter++) {
+            CHECK( x[x_iter] == Approx(x_out_expected[x_iter]).epsilon(EPSILON) );
+        }
+        
+        // inverse matrix multiplication
+        butterfly_matrix_multiplication(x,
+                                        thetas_inv,
+                                        HALFBLOCKLENGTH,
+                                        0,
+                                        0,
+                                        mat_idx);
+        for (x_iter=0; x_iter<BLOCKLENGTH; x_iter++) {
+            CHECK( x[x_iter] == Approx(x_final_expected[x_iter]).epsilon(EPSILON) );
+        }
+    }
+
+    SECTION( "mat_idx = 2" ) {
+        int mat_idx = 2;
+
+        // matrix multiplication
+        double x_out_expected[BLOCKLENGTH] = {
+            0.9946711652, 1.3972721725,
+            1.1048209236, -1.8594704827,
+            0.3910538556, 1.9318614356,
+            -0.9257844685, 5.7024478422,
+            0.2543293617, 1.9800000888,
+            1.6202828691, -0.2993320038,
+            0.1047186374, -1.8756102881,
+            0.4347804124, -1.1883122685};
+        butterfly_matrix_multiplication(x,
+                                        thetas,
+                                        HALFBLOCKLENGTH,
+                                        0,
+                                        0,
+                                        mat_idx);
+        for (x_iter=0; x_iter<BLOCKLENGTH; x_iter++) {
+            CHECK( x[x_iter] == Approx(x_out_expected[x_iter]).epsilon(EPSILON) );
+        }
+        
+        // inverse matrix multiplication
+        butterfly_matrix_multiplication(x,
+                                        thetas_inv,
+                                        HALFBLOCKLENGTH,
+                                        0,
+                                        0,
+                                        mat_idx);
+        for (x_iter=0; x_iter<BLOCKLENGTH; x_iter++) {
+            CHECK( x[x_iter] == Approx(x_final_expected[x_iter]).epsilon(EPSILON) );
+        }
+    }
+}
