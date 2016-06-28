8bff00822a0d3db666114603e497edc7a59b95f8
albertno <albert.no0@gmail.com>
2016-06-27 23:25:14 -0700

    TST: big test

--- catch-tests/test_CROM_big.cpp
+++ catch-tests/test_CROM_big.cpp
@@ -1,5 +1,8 @@
-#define TEST_BLOCKLENGTH 8
-#define TEST_HALFBLOCKLENGTH 4
+#include <ctime>
+
+#define TEST_BLOCKLENGTH 65536
+#define TEST_HALFBLOCKLENGTH 32767
+#define SRAND_SEED 5298
 #define EPSILON 1e-6
 
 #include "catch.hpp"
@@ -9,47 +12,45 @@
 using namespace std;
 
 TEST_CASE("CROM_full small big", "[CROM_full]") {
-    double R = 1;
+    double R = 1.0;
     int xdim = TEST_BLOCKLENGTH;
-    double x[TEST_BLOCKLENGTH] = {
-        -0.769811, 0.186957,
-        -0.0782989, 0.446422,
-        -0.4684, 0.0454221,
-        1.56657, 1.44174};
-    double x_save[TEST_BLOCKLENGTH] = {
-        -0.769811, 0.186957,
-        -0.0782989, 0.446422,
-        -0.4684, 0.0454221,
-        1.56657, 1.44174};
-    double xhat_expected[TEST_BLOCKLENGTH] = {
-        -0.4883347668, 0.3162271728,
-        -0.0086511553, 0.375842363,
-        -0.5104244858, 0.1219668209,
-        0.23900423, 0.825249401};
+    srand(SRAND_SEED);
+    double x[TEST_BLOCKLENGTH];
+    double x_save[TEST_BLOCKLENGTH];
     bool verbose = false;
     double xhat[TEST_BLOCKLENGTH];
-    string name = "full_test";
+    string name = "full_big_test";
 
     CROM_encoder enc (name, xdim, R, verbose);
     int x_iter;
+    double uniform1, uniform2;
+    double normal1, normal2;
+    for (x_iter=0; x_iter<xdim/2; x_iter++) {
+        uniform1 = static_cast <double> (rand()) /static_cast <double> (RAND_MAX);
+        uniform2 = static_cast <double> (rand()) /static_cast <double> (RAND_MAX);
+        normal1 = sqrt(-2*log(uniform1)) * cos(2*M_PI*uniform2);
+        normal2 = sqrt(-2*log(uniform1)) * sin(2*M_PI*uniform2);
+        x[2*x_iter] = x_save[2*x_iter] = normal1;
+        x[2*x_iter+1] = x_save[2*x_iter+1] = normal2;
+    }
 
-    SECTION( "write and read m_array using binary file" ) {
+
+    SECTION( "compare final l2 norm after big run" ) {
         // read and store original x
         enc.set_x(x);
         enc.copy_x(x_save);
 
         // check L
         int L = enc.get_L();
-        CHECK( L == 3 );
+        CHECK( L == 5909 );
 
-        // encoding and check m_array
+        std::clock_t enc_time;
+        printf("BIG test: xdim = %d, and R = %f\n", xdim, R);
+        // encoding
+        enc_time = std::clock();
         enc.run();
-        int *m_array_copy = new int[L];
-        enc.copy_m_array(m_array_copy);
-        std::vector<int> v_m_arr(m_array_copy, m_array_copy+L);
-        std::vector<int> v_m_arr_expected {3, 1, 1};
-        CHECK( v_m_arr == v_m_arr_expected );
-        delete[] m_array_copy;
+        enc_time = std::clock() - enc_time;
+        printf("Encoding took %f seconds \n", ((double)enc_time)/(double)CLOCKS_PER_SEC);
 
         // write_m_array in binary file
         enc.write_m_array(true);
@@ -58,12 +59,13 @@ TEST_CASE("CROM_full small big", "[CROM_full]") {
         CROM_decoder dec (name, xdim, L, verbose);
         dec.read_m_array(true);
 
+        std::clock_t dec_time;
         // decoding
+        dec_time = clock();
         dec.run();
+        dec_time = clock() - dec_time;
+        printf("Decoding took %f seconds \n", ((double)dec_time)/(double)CLOCKS_PER_SEC);
         dec.copy_x_hat(xhat);
-        for (x_iter=0; x_iter< xdim; x_iter++) {
-            CHECK( xhat[x_iter] == Approx(xhat_expected[x_iter]).epsilon(EPSILON) );
-        }
 
         // check write_x_hat
         dec.write_x_hat();
@@ -72,7 +74,10 @@ TEST_CASE("CROM_full small big", "[CROM_full]") {
         double l2dist;
         l2dist = compute_l2_dist(x_save, xhat, xdim);
         l2dist /= static_cast<double> (xdim);
-        CHECK( l2dist == Approx(0.2819860781) );
+        double *l2_array_copy = new double[L];
+        enc.copy_l2_array(l2_array_copy);
+
+        CHECK( l2dist == Approx(l2_array_copy[L-1]) );
     }
 }
 
