dff8cf4469480efe61df5cb75db700fc8f022bd9
Albert <albert.no0@gmail.com>
2018-02-18 17:11:05 +0900

    ENH: add decoding part to example main code

--- example/main.cpp
+++ example/main.cpp
@@ -10,36 +10,15 @@
 #include "../cromq/cromq_decoder.hpp"
 #include "../cromq/cromq_encoder.hpp"
 
-int main() {
-    // Setup parameters here
-    // *********************
-    std::string fname = "SRR494099.qscore";
-    int xdim = 65536;
-    int num_x = 36;
-    double rd_param = 1.4;
-    bool verbose = false;
-    // double R_enc = 2;
-    double R_enc = 2;
-    int num_dec_pts = 4;
-    // std::vector<double> R_dec = {0.5, 1, 1.5, 2};
-    // *********************
 
-    if (fname.substr(fname.size()-7, fname.size()) != ".qscore") {
-        std::cout << "Wrong input file name" << std::endl;
-        exit(1);
-    }
+int generate_subqscore_files(std::string name, std::string fname, std::vector<std::string> &subfnames, int xdim) {
+    int line_idx = 0;
+    int file_idx = 0;
 
-    std::string name = fname.substr(0, fname.size()-7);
-
-    // split files
     std::ifstream qscore_file(fname);
     std::string line;
     std::stringstream subfname_tmp;
 
-    std::vector<std::string> subfnames;
-
-    int line_idx = 0;
-    int file_idx = 0;
     bool end_indicator = true;
     std::string subfname;
 
@@ -66,17 +45,89 @@ int main() {
     }
     qscore_file.close();
 
-    // XXX TBD take care of remainder (also when running cromq in the below)
+    return file_idx-1;
+}
 
-    // Run CROMq
-    std::clock_t run_time;
-    for (int cromq_idx=0; cromq_idx<file_idx-1; cromq_idx++) {
-        run_time = std::clock();
-        std::cout << "Processing " << subfnames[cromq_idx] << std::endl;
-        CROMq_encoder enc(name, subfnames[cromq_idx], cromq_idx, num_x, xdim, rd_param, R_enc, verbose);
-        enc.run();
-        run_time = std::clock() - run_time;
-        std::cout << "It took " << ((double)run_time / (double)CLOCKS_PER_SEC) << " seconds" << std::endl << std::endl;
+
+void get_subfnames(std::string name, std::vector<std::string> &subfnames, int file_idx) {
+    std::stringstream subfname_tmp;
+    std::string subfname;
+
+    for (int iter_idx=0; iter_idx<file_idx; iter_idx++) {
+        subfname_tmp.str(std::string());
+        subfname_tmp << std::setw(4) << std::setfill('0') << iter_idx;
+        subfname = "split_" + name + "_" + subfname_tmp.str() + ".subqscores";
+        subfnames.push_back(subfname);
+    }
+}
+
+
+int main() {
+    // Setup encoding parameters here
+    // *********************
+    std::string fname = "SRR494099.qscore";
+    int xdim = 65536;
+    int num_x = 36;
+    double rd_param = 1.4;
+    double R_enc = 0.1;
+    char mode = 'd';
+    bool verbose = false;
+    // *********************
+
+    // Setup decoding parameters here
+    // *********************
+    int file_idx = 2;
+    int num_dec_pts = 2;
+    std::vector<double> R_dec = {0.05, 0.09};
+    // *********************
+
+    if (fname.substr(fname.size()-7, fname.size()) != ".qscore") {
+        std::cout << "Wrong input file name" << std::endl;
+        exit(1);
+    }
+    std::string name = fname.substr(0, fname.size()-7);
+
+    switch(mode) {
+        case 'e': {
+            // split files
+            std::vector<std::string> subfnames;
+            file_idx = generate_subqscore_files(name, fname, subfnames, xdim);
+
+            // XXX TBD take care of remainder (also when running cromq in the below)
+
+            // Run CROMq encoder
+            std::clock_t run_time;
+            for (int cromq_idx=0; cromq_idx<file_idx; cromq_idx++) {
+                run_time = std::clock();
+                std::cout << "Processing " << subfnames[cromq_idx] << std::endl;
+                CROMq_encoder enc(name, subfnames[cromq_idx], cromq_idx, num_x, xdim, rd_param, R_enc, verbose);
+                enc.run();
+                run_time = std::clock() - run_time;
+                std::cout << "It took " << ((double)run_time / (double)CLOCKS_PER_SEC) << " seconds" << std::endl << std::endl;
+            }
+        } break;
+
+        case 'd': {
+            // Run CROMq decoder
+            std::clock_t run_time;
+            std::vector<std::string> subfnames;
+            get_subfnames(name, subfnames, file_idx);
+            for (int cromq_idx=0; cromq_idx<file_idx; cromq_idx++) {
+                for (int dec_idx=0; dec_idx<num_dec_pts; dec_idx++) {
+                    run_time = std::clock();
+                    std::cout << "Processing " << subfnames[cromq_idx] << " at rate = " << R_dec[dec_idx] << std::endl;
+                    CROMq_decoder dec(name, subfnames[cromq_idx], cromq_idx, num_x, xdim, rd_param, R_dec[dec_idx], verbose);
+                    dec.run();
+                    run_time = std::clock() - run_time;
+                    std::cout << "It took " << ((double)run_time / (double)CLOCKS_PER_SEC) << " seconds" << std::endl << std::endl;
+                }
+            }
+        } break;
+
+        default: {
+            std::cout << "Wrong mode (it should be either `e` (encoding) or `d` (decoding)" << std::endl;
+            exit(1);
+        }
     }
 
     return 0;
